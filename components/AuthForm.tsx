15f0b053ce77f4b08af41813cbf8fca26eb39f9f
Mike <binarybardcode@gmail.com>
2025-01-26 20:19:42 +0200

    Add OTP verification modal and input components

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,6 +17,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { createAccount } from "@/lib/actions/user.actions";
+import OTPModal from "./OTPModal";
 
 
 type FormType = "sign-in" | "sign-up";
@@ -124,6 +125,9 @@ const AuthForm = ({ type } : { type: FormType }) => {
     </Form>
 
     {/* OTP VERIFICATION */}
+    {accountId && (
+        <OTPModal email={form.getValues("email")} accountId={accountId} />
+        )}
     </>
   )
 }
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { ID, Query } from "node-appwrite";
 import { createAdminClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { parseStringify } from "../utils";
+import { cookies } from "next/headers";
 
 const getUserByEmail = async (email: string) => {
     const { databases } = await createAdminClient();
@@ -21,7 +22,7 @@ const handleError = (error: unknown, message: string)  => {
     throw error;
 } 
 
-const sendEmailOTP = async ({ email } : { email: string }) => {
+export const sendEmailOTP = async ({ email } : { email: string }) => {
     const { account } = await createAdminClient();
 
     try {
@@ -57,4 +58,25 @@ export const createAccount = async ({ fullName, email }: { fullName: string, ema
 
     return parseStringify({ accountId });
 
+}
+
+export const verifySecret = async ({ accountId, password }: { accountId: string, password: string }) => {
+    try {
+        const { account } = await createAdminClient();
+
+        const session = await account.createSession(accountId, password);
+
+        (await cookies()).set('appwrite-session', session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+        });
+
+        return parseStringify({ sessionId: session.$id });
+    } catch (error) {
+
+        handleError(error, "Failed to verify OTP");
+
+    }
 }
