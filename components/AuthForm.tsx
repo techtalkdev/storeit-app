4edfcdfaae056089f8a99d00baa6115b3510ff3c
Mike <binarybardcode@gmail.com>
2025-01-26 19:03:07 +0200

    Implement account creation functionality and add Appwrite configuration

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -16,6 +16,7 @@ import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { createAccount } from "@/lib/actions/user.actions";
 
 
 type FormType = "sign-in" | "sign-up";
@@ -31,6 +32,7 @@ const authFormSchema = (formType: FormType) => {
 const AuthForm = ({ type } : { type: FormType }) => {
     const [isLoading, setIsLoading] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
+    const [accountId, setAccountId] = useState(null);
 
     const formSchema = authFormSchema(type);
 
@@ -42,8 +44,21 @@ const AuthForm = ({ type } : { type: FormType }) => {
     },
   })
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values)
-  }
+    setIsLoading(true);
+    setErrorMessage("");
+
+    try {
+        const user = await createAccount({ 
+            fullName: values.fullName || "",
+            email: values.email
+        });
+        setAccountId(user.accountId);
+    } catch (error) {
+        setErrorMessage("Failed to create account. Please try again.");
+    } finally {
+        setIsLoading(false);
+    }
+}; 
 
   return (
     <>
